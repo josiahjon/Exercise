67ecb2801d097ebd4d38a817cd6b103d3cace37a
josiahjon <josiahtalebi@gmail.com>
2023-03-24 10:58:02 -0700

    Add project files.

---
+++ ExerciseWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ExerciseWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExerciseWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
