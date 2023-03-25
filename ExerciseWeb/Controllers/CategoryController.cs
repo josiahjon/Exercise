d6c6dae40c8a96533854cdd57f222a8401bc89fd
josiahjon <josiahtalebi@gmail.com>
2023-03-24 20:54:05 -0700

    Retrieve categories function implemented

---
+++ ExerciseWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ExerciseWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciseWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
