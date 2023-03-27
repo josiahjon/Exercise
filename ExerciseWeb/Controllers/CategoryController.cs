c1ced585beb38b68b7c4d0354bc07b46b542b155
josiahjon <josiahtalebi@gmail.com>
2023-03-26 17:35:17 -0700

    adding exercise now adds to sql database

--- ExerciseWeb/Controllers/CategoryController.cs
+++ ExerciseWeb/Controllers/CategoryController.cs
@@ -17,5 +17,22 @@ namespace ExerciseWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
