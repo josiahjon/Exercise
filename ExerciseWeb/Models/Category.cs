b70a70d7830c074ad3e48edd1dfc622c5027ca3b
josiahjon <josiahtalebi@gmail.com>
2023-03-26 18:00:37 -0700

    added client side validation

--- ExerciseWeb/Models/Category.cs
+++ ExerciseWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExerciseWeb.Models
 {
@@ -8,6 +9,7 @@ namespace ExerciseWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
