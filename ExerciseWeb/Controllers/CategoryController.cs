c38da6c15fd9cc09ddee391fb5a7bc5e55f7f436
josiahjon <josiahtalebi@gmail.com>
2023-03-28 20:26:27 -0700

    delete exercise functionality added

--- ExerciseWeb/Controllers/CategoryController.cs
+++ ExerciseWeb/Controllers/CategoryController.cs
@@ -42,7 +42,7 @@ namespace ExerciseWeb.Controllers
             }
             return View(obj);
         }
-
+        //get
         public IActionResult Edit(int? id)
         {
             if(id == null || id == 0)
@@ -76,6 +76,39 @@ namespace ExerciseWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
         }
     }
 }
